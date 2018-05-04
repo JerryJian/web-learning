f754bc8851dd3df72316df366cd7c21426398902
Jerry Jian <jerry.jian@xintechs.com>
2018-05-04 22:10:06 +0800

    Append message to message list when send

--- wechat/2/js/index.js
+++ wechat/2/js/index.js
@@ -1,57 +1,87 @@
-$(function () {  
-    var _editArea = $('#editArea');  
-  
+$(function () {
+    var _editArea = $('#editArea');
+
     //显示隐藏发送按钮  
-    var _editAreaInterval;  
-    $('#editArea').focus(function () {  
-        var _this = $(this), html;  
-        _editAreaInterval = setInterval(function () {  
-            html = _this.html();  
-            if (html.length > 0) {  
-                $('#web_wechat_pic').hide();  
-                $('#btn_send').show();  
-            } else {  
-                $('#web_wechat_pic').show();  
-                $('#btn_send').hide();  
-            }  
-        }, 200);  
-    });  
-  
-    $('#editArea').blur(function () {  
-        clearInterval(_editAreaInterval);  
-    });  
-  
+    var _editAreaInterval;
+    $('#editArea').focus(function () {
+        var _this = $(this),
+            html;
+        _editAreaInterval = setInterval(function () {
+            html = _this.html();
+            if (html.length > 0) {
+                $('#web_wechat_pic').hide();
+                $('#btn_send').show();
+            } else {
+                $('#web_wechat_pic').show();
+                $('#btn_send').hide();
+            }
+        }, 200);
+    });
+
+    $('#editArea').blur(function () {
+        clearInterval(_editAreaInterval);
+    });
+
     //显示隐藏表情栏  
-    $('.web_wechat_face').click(function () {  
-        $('.box_ft_bd').toggleClass('hide');  
-        resetMessageArea();  
-    });  
-  
+    $('.web_wechat_face').click(function () {
+        $('.box_ft_bd').toggleClass('hide');
+        resetMessageArea();
+    });
+
     //切换表情主题  
-    $('.exp_hd_item').click(function () {  
-        var _this = $(this), i = _this.data('i');  
-        $('.exp_hd_item,.exp_cont').removeClass('active');  
-        _this.addClass('active');  
-        $('.exp_cont').eq(i).addClass('active');  
-        resetMessageArea();  
-    });  
-  
+    $('.exp_hd_item').click(function () {
+        var _this = $(this),
+            i = _this.data('i');
+        $('.exp_hd_item,.exp_cont').removeClass('active');
+        _this.addClass('active');
+        $('.exp_cont').eq(i).addClass('active');
+        resetMessageArea();
+    });
+
     //选中表情  
-    $('.exp_cont a').click(function () {  
-        var _this = $(this);  
-        var html = '<img class="' + _this[0].className + '" title="' + _this.html() + '" src="images/spacer.gif">';  
-        _editArea.html(_editArea.html() + html);  
-        $('#web_wechat_pic').hide();  
-        $('#btn_send').show();  
-    });  
-  
-    resetMessageArea();  
-  
-  
-    function sendMsg(str) {  
-    }  
-  
-    function resetMessageArea() {  
-        $('#messageList').animate({ 'scrollTop': 999 }, 500);  
-    }  
-});  
+    $('.exp_cont a').click(function () {
+        var _this = $(this);
+        var html = '<img class="' + _this[0].className + '" title="' + _this.html() + '"/>';
+        _editArea.html(_editArea.html() + html);
+        $('#web_wechat_pic').hide();
+        $('#btn_send').show();
+    });
+
+    resetMessageArea();
+
+
+    $('#btn_send').click(function () {
+        sendMessage();
+    });
+
+    
+
+    function sendMessage() {
+        var html = _editArea.html();
+        var message = '<div class="message me">' +
+            '<img class="avatar" src="images/hyf.jpg" />' +
+            '<div class="content">' +
+            '<div class="nickname">' +
+            '<span class="time">10:10:08</span>' +
+            '</div>' +
+            '<div class="bubble bubble_primary right">' +
+            '<div class="bubble_cont">' +
+            '<div class="plain">' +
+            '<pre>' + _editArea.html() + '</pre>' +
+            '</div>' +
+            '</div>' +
+            '</div>' +
+            '</div>' +
+            '</div>';
+        $('#messageList').append(message);
+        resetMessageArea();
+        _editArea.html('');
+    }
+
+    function resetMessageArea() {
+        $('#messageList').animate({
+            'scrollTop': 999
+        }, 500);
+    }
+
+});
